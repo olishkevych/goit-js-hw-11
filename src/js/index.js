5ba40c2c5f8145327bed3b1a1099f78450ad6f02
olishkevych <olishkevych.natalia@gmail.com>
2023-05-15 08:19:05 +0200

    add load more button

--- src/js/index.js
+++ src/js/index.js
@@ -1,53 +1,51 @@
 const formEl = document.getElementById('search-form');
 const galleryEl = document.querySelector('.gallery');
+const loadMoreBtn = document.querySelector('.load-more');
 
 const API_KEY = '36396693-28c70313af4bfc02da8bd4331';
 const URL = 'https://pixabay.com/api/';
+let page;
+let searchQuery = '';
+const per_page = 40;
+let markup = '';
 
 formEl.addEventListener('submit', onFormSubmit);
 
 function onFormSubmit(event) {
   event.preventDefault();
   galleryEl.innerHTML = '';
-  let searchQuery = formEl.elements[0].value.trim();
+  loadMoreBtn.classList.add('hidden');
+
+  searchQuery = formEl.elements[0].value.trim();
   if (searchQuery) {
-    fetchImages(searchQuery)
-      .then(images => {
-        if (images.total === 0) {
-          alert(
-            'Sorry, there are no images matching your search query. Please try again.'
-          );
-        } else {
-          renderImages(createImagesMarkup(images));
-        }
-      })
-      .catch(error => console.log(error));
+    creatFetchRequest();
   } else {
     alert('empty field');
     return;
   }
-
   formEl.reset();
 }
 
 async function fetchImages(searchQuery) {
+  page += 1;
   const options = {
     q: searchQuery,
     image_type: 'photo',
     orientation: 'horizontal',
     safesearch: true,
+    page: page,
+    per_page: per_page,
   };
 
   const parameters = new URLSearchParams(options);
 
   const response = await fetch(`${URL}?key=${API_KEY}&${parameters}`);
   const images = await response.json();
-  console.log(parameters.q);
   return images;
 }
 
 function createImagesMarkup(images) {
-  const markup = images.hits
+  markup = images.hits
     .map(
       ({
         webformatURL,
@@ -84,4 +82,27 @@ function createImagesMarkup(images) {
 
 function renderImages(markup) {
   galleryEl.insertAdjacentHTML('beforeend', markup);
+}
+
+function onLoadMoreClick() {
+  fetchImages(searchQuery);
+  renderImages(markup);
+}
+
+function creatFetchRequest() {
+  page = 0;
+  fetchImages(searchQuery)
+    .then(images => {
+      if (images.total === 0) {
+        alert(
+          'Sorry, there are no images matching your search query. Please try again.'
+        );
+      } else {
+        createImagesMarkup(images);
+        renderImages(markup);
+        loadMoreBtn.classList.remove('hidden');
+        loadMoreBtn.addEventListener('click', onLoadMoreClick);
+      }
+    })
+    .catch(error => console.log(error));
 }
